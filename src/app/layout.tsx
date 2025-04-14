fcc3d1a297c9241df59975be9a57655d6da189e6
Mayur Bhatt <mayur27021991@gmail.com>
2025-04-14 12:51:46 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,8 +10,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: `Next.js Blog Example with ${CMS_NAME}`,
-  description: `A statically generated blog example using Next.js and ${CMS_NAME}.`,
+  title: `iMRS 2000 Blogs`,
+  description: `iMRS 2000 Blogs about PEMF Therapy, Machine, OMNIUM1, PEMF Mat and Devices. `,
   openGraph: {
     images: [HOME_OG_IMAGE_URL],
   },
